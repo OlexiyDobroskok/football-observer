3010c18a0719ded9e79d06d4213107206713b22d
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-03-30 11:03:40 +0300

    Initial project

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias:{
+      'src': path.resolve(__dirname,'./src'),
+      'components': path.resolve(__dirname,'./src/components'),
+      'assets': path.resolve(__dirname,'./src/assets'),
+      'hooks': path.resolve(__dirname,'./src/hooks'),
+      'modules': path.resolve(__dirname,'./src/modules'),
+      'pages': path.resolve(__dirname,'./src/pages'),
+      'store': path.resolve(__dirname,'./src/store'),
+      'UI': path.resolve(__dirname,'./src/UI'),
+    }
+  }
+})
