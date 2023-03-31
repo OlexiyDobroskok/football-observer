592dada05578101d7d36e58a342f6e64ec2b3571
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-03-31 17:27:25 +0300

    The init corrections

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import {StrictMode} from 'react'
-import ReactDOM from 'react-dom/client'
-import 'styles/index.scss'
-import App from "src/App";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "styles/index.scss";
+import { App } from "src/app";
 
-
-
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
