1079bc8dd8cbd82c42ad352dbe1795f3cc10c124
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-04-01 15:54:05 +0300

    Added UI component "List"

---
+++ src/ui/list/list.tsx
@@ -0,0 +1,20 @@
+import classes from "./list.module.scss";
+import { ReactNode } from "react";
+
+interface ListProps<T> {
+  listItems: T[];
+  renderItem: (item: T) => ReactNode;
+  className?: string;
+}
+
+export const List = <T extends unknown>({
+  listItems,
+  renderItem,
+  className,
+}: ListProps<T>) => {
+  return (
+    <ul className={[classes.list, className].join(" ")}>
+      {listItems.map(renderItem)}
+    </ul>
+  );
+};
