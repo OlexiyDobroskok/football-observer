ca5601ee4570628c94814dcf71ff3f5df187351c
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-04-12 20:02:38 +0300

    Added public API for the leagues filter module

---
+++ src/modules/leagues-filter/index.ts
@@ -0,0 +1,5 @@
+export { leaguesReducer } from "./store/leagues-slice";
+export { SearchLeague } from "./components/search-league/search-league";
+export { SearchModal } from "./components/search-modal/search-modal";
+export { SeasonsSelect } from "./components/seasons-select/seasons-select";
+export { TopLeaguesList } from "./components/top-leagues-list/top-leagues-list";
