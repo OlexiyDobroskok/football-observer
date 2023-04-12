34f40a640c3263d1a172f3f81e5611e048a848d6
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-04-12 19:56:17 +0300

    Created SearchLeague component of the leagues filter module

---
+++ src/modules/leagues-filter/components/search-league/search-league.tsx
@@ -0,0 +1,68 @@
+import { ChangeEvent, useEffect, useMemo, useRef } from "react";
+import { useAppDispatch, useAppSelector } from "hooks/redux";
+import { SearchInput } from "ui/search-input/search-input";
+import { List } from "ui/list/list";
+import { LeagueButton } from "modules/leagues-filter/ui/league-button/league-button";
+import {
+  setCurrentLeague,
+  setSearchQuery,
+} from "modules/leagues-filter/store/leagues-slice";
+import { closeModal } from "store/modals-slice";
+import classes from "./search-league.module.scss";
+
+export const SearchLeague = () => {
+  const { availableLeagues, searchQuery } = useAppSelector(
+    ({ leagues }) => leagues
+  );
+  const dispatch = useAppDispatch();
+  const searchInputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    searchInputRef.current?.focus();
+  }, []);
+
+  const foundLeagues = useMemo(() => {
+    const minLength = 3;
+    return availableLeagues && searchQuery.length >= minLength
+      ? availableLeagues.filter(({ league, country }) =>
+          `${league.name} ${country.name}`
+            .toLowerCase()
+            .includes(searchQuery.trim().toLowerCase())
+        )
+      : [];
+  }, [availableLeagues, searchQuery]);
+
+  const handleQueryChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    dispatch(setSearchQuery(target.value));
+  };
+
+  const handleLeagueButtonClick = (id: number) => {
+    dispatch(setCurrentLeague(id));
+    dispatch(setSearchQuery(""));
+    dispatch(closeModal());
+  };
+
+  return (
+    <div className={classes.search}>
+      <SearchInput
+        ref={searchInputRef}
+        id="search-league"
+        placeholder="League, cup, country"
+        value={searchQuery}
+        onChange={handleQueryChange}
+      />
+      <List
+        className={classes["result-list"]}
+        listItems={foundLeagues}
+        renderItem={({ league: { id, name } }) => (
+          <LeagueButton
+            key={id}
+            id={id}
+            leagueName={name}
+            onClick={() => handleLeagueButtonClick(id)}
+          />
+        )}
+      />
+    </div>
+  );
+};
