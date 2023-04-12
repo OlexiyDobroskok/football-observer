3f14cf1282cecb85a2bbddf4e587d40ac9791a66
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-04-12 20:16:14 +0300

    Added modals and leagues reducers

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { standingsReducer } from "modules/standings";
+import { leaguesReducer } from "modules/leagues-filter/store/leagues-slice";
+import { modalsReducer } from "store/modals-slice";
 
 export const store = configureStore({
   reducer: {
+    modals: modalsReducer,
+    leagues: leaguesReducer,
     standings: standingsReducer,
   },
 });
