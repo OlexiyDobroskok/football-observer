b089ae910bb64efe898d03782c1dcb5849d3606c
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-04-12 19:20:26 +0300

    Created modals slice

---
+++ src/store/modals-slice.ts
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface ModalsState {
+  searchModalIsOpen: boolean;
+}
+
+const initialState: ModalsState = {
+  searchModalIsOpen: false,
+};
+
+const modalsSlice = createSlice({
+  name: "modals",
+  initialState,
+  reducers: {
+    openSearchModal: (state) => {
+      state.searchModalIsOpen = true;
+    },
+    closeModal: (state) => {
+      (Object.keys(state) as Array<keyof ModalsState>).forEach((modal) => {
+        state[modal] = false;
+      });
+    },
+  },
+});
+
+export const { openSearchModal, closeModal } = modalsSlice.actions;
+export const uiReducer = modalsSlice.reducer;
