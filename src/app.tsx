e09d82f6366d616bbca5791a1fedc04e5d92a2bb
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-04-12 21:23:54 +0300

    Initial App structure

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,27 @@
-export const App = () => {
-  return <>hello</>;
+import { FC } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Root, Calendar, Home, Profile, Standings } from "src/pages";
+import { PAGE_ROUTES } from "helpers/consts";
+import { Provider } from "react-redux";
+import { store } from "store/store";
+
+export const App: FC = () => {
+  const router = createBrowserRouter([
+    {
+      path: PAGE_ROUTES.ROOT,
+      element: <Root />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: PAGE_ROUTES.CALENDAR, element: <Calendar /> },
+        { path: PAGE_ROUTES.STANDINGS, element: <Standings /> },
+        { path: PAGE_ROUTES.PROFILE, element: <Profile /> },
+      ],
+    },
+  ]);
+
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
 };
