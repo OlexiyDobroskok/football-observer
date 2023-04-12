eb15ccf4c6675e0f8c7322caf9a6e132a3a94e2d
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-04-12 21:04:55 +0300

    Created LeaguesFilter module and updated public API it

--- src/modules/leagues-filter/index.ts
+++ src/modules/leagues-filter/index.ts
@@ -1,5 +1,3 @@
 export { leaguesReducer } from "./store/leagues-slice";
-export { SearchLeague } from "./components/search-league/search-league";
-export { SearchModal } from "./components/search-modal/search-modal";
+export { LeaguesFilter } from "./leagues-filter";
 export { SeasonsSelect } from "./components/seasons-select/seasons-select";
-export { TopLeaguesList } from "./components/top-leagues-list/top-leagues-list";
---
+++ src/modules/leagues-filter/leagues-filter.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "hooks/redux";
+import { openSearchModal } from "store/modals-slice";
+import { fetchLeagues } from "modules/leagues-filter/store/fetch-leagues-thunk";
+import { SearchModal } from "modules/leagues-filter/components/search-modal/search-modal";
+import { TopLeaguesList } from "modules/leagues-filter/components/top-leagues-list/top-leagues-list";
+import { Container } from "ui/container/container";
+import { SearchButton } from "ui/search-button/search-button";
+import classes from "./leagues-filter.module.scss";
+
+export const LeaguesFilter = () => {
+  const { searchModalIsOpen } = useAppSelector(({ modals }) => modals);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchLeagues({}));
+  }, []);
+
+  const handleSearchButtonClick = () => {
+    dispatch(openSearchModal());
+  };
+
+  return (
+    <>
+      {searchModalIsOpen && <SearchModal />}
+      <Container className={classes.container}>
+        <SearchButton onClick={handleSearchButtonClick} />
+      </Container>
+      <TopLeaguesList />
+    </>
+  );
+};
