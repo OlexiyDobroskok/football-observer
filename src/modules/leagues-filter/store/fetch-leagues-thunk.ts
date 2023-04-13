8c6fe4661caa341ae1ca5e6f0a0d183a3f1a5a1c
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-04-13 15:41:41 +0300

    Restructured fetchLeague thunk

--- src/modules/leagues-filter/store/fetch-leagues-thunk.ts
+++ src/modules/leagues-filter/store/fetch-leagues-thunk.ts
@@ -1,33 +1,20 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { LeagueInformation } from "modules/leagues-filter/types/types";
-import { footballApi, FootballApiResponse } from "api/football-service";
+import { LeagueInformation } from "modules/leagues-filter/api/types";
+import {
+  FetchLeaguesArgs,
+  getAllLeagues,
+} from "modules/leagues-filter/api/endpoints";
 
 type FetchLeaguesError = string;
-interface FetchLeaguesArgs {
-  id: number;
-  type: "league" | "cup";
-  current: "true" | "false";
-}
 
 export const fetchLeagues = createAsyncThunk<
   LeagueInformation[],
-  Partial<FetchLeaguesArgs>,
+  FetchLeaguesArgs,
   { rejectValue: FetchLeaguesError }
->(
-  "leagues-filter/fetchLeagues",
-  async ({ id, type, current }, { rejectWithValue }) => {
-    try {
-      const { data }: FootballApiResponse<LeagueInformation[]> =
-        await footballApi.get("leagues", {
-          params: {
-            id: id,
-            type: type,
-            current: current,
-          },
-        });
-      return data.response;
-    } catch (error) {
-      return rejectWithValue((error as Error).message);
-    }
+>("leagues-filter/fetchLeagues", async ({ type }, { rejectWithValue }) => {
+  try {
+    return await getAllLeagues({ type });
+  } catch (error) {
+    return rejectWithValue((error as Error).message);
   }
-);
+});
