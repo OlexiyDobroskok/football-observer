0050ca4ed29e39554eeaf815c418664bd4edf692
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-04-23 20:01:17 +0300

    Corrected styles SearchButton component

--- src/ui/SearchButton/SearchButton.tsx
+++ src/ui/SearchButton/SearchButton.tsx
@@ -1,6 +1,5 @@
 import { MouseEventHandler } from "react";
 import { SvgIcon } from "src/ui/SvgIcon/SvgIcon";
-import { Button } from "ui/Button/Button";
 import searchIcon from "./search.svg";
 import classes from "./SearchButton.module.scss";
 
@@ -9,7 +8,7 @@ interface SearchButtonProps {
 }
 
 export const SearchButton = ({ onClick }: SearchButtonProps) => (
-  <Button className={classes.button} onClick={onClick}>
+  <button className={classes.button} onClick={onClick}>
     <SvgIcon className={classes.icon} href={`${searchIcon}#search`} />
-  </Button>
+  </button>
 );
