d6ca1f0f1d16cf96821aca23d461e03f248d2b0d
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-04-23 13:14:57 +0300

    Refactored functions and add getValidDateTimeStrYMDFormat function for dateTime attribute

--- src/modules/fixtures/helpers/date-format.ts
+++ src/modules/fixtures/helpers/date-format.ts
@@ -1,24 +1,35 @@
-const getWeekDayLF = (date: Date) =>
-  date.toLocaleString("en", { weekday: "long" });
+const getFormattedDate = (date: Date, opt: Intl.DateTimeFormatOptions) =>
+  date.toLocaleString("en", opt);
 
-const getMonthLF = (date: Date) => date.toLocaleString("en", { month: "long" });
+const getWeekDayLF = (date: Date) =>
+  getFormattedDate(date, { weekday: "long" });
+
+const getMonthLF = (date: Date) => getFormattedDate(date, { month: "long" });
+
+const getMonthSF = (date: Date) => getFormattedDate(date, { month: "2-digit" });
 
 const getMonthDaySF = (date: Date) =>
-  date.toLocaleString("en", { day: "numeric" });
+  getFormattedDate(date, { day: "numeric" });
+
+const getMonthDayLF = (date: Date) =>
+  getFormattedDate(date, { day: "2-digit" });
+
+export const getWeekDayShortFormat = (date: Date) =>
+  getFormattedDate(date, { weekday: "short" });
+
+export const getDayAndMonthLongFormat = (date: Date) =>
+  getFormattedDate(date, { month: "long", day: "numeric" });
+
+export const getTimeShortFormat = (date: Date) =>
+  getFormattedDate(date, {
+    timeStyle: "short",
+    hour12: false,
+  });
 
 export const getDateLongFormat = (date: Date) =>
   `${getWeekDayLF(date)} ${getMonthDaySF(date)} ${getMonthLF(
     date
   )} ${date.getFullYear()}`;
 
-export const getTimeShortFormat = (date: Date) =>
-  date.toLocaleString("en", {
-    timeStyle: "short",
-    hour12: false,
-  });
-
-export const getWeekDayShortFormat = (date: Date) =>
-  date.toLocaleString("en", { weekday: "short" });
-
-export const getDayAndMonthLongFormat = (date: Date) =>
-  date.toLocaleString("en", { month: "long", day: "numeric" });
+export const getValidDateTimeStrYMDFormat = (date: Date) =>
+  `${date.getFullYear()}-${getMonthSF(date)}-${getMonthDayLF(date)}`;
