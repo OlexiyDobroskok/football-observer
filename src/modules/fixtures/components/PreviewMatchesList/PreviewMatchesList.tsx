a013e1c50c51a9b8c7e8cfdbdaab6fc15449a05e
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-04-23 22:42:15 +0300

    Rename LiveMatchesList to PreviewMatchesList. Added matchesLimit prop and added style for situation, when list have only single match

---
+++ src/modules/fixtures/components/PreviewMatchesList/PreviewMatchesList.tsx
@@ -0,0 +1,33 @@
+import { Fixture } from "api/types/fixtures-types";
+import { FixturePreviewCard } from "modules/fixtures/components/FixturePreviewCard/FixturePreviewCard";
+import classes from "./PreviewMatchesList.module.scss";
+
+export interface PreviewMatchesListProps {
+  matches: Fixture[];
+  matchesLimit?: number;
+}
+
+export const PreviewMatchesList = ({
+  matches,
+  matchesLimit,
+}: PreviewMatchesListProps) => {
+  const matchesList = matches.slice(0, matchesLimit).map((matchInfo) => (
+    <div className={classes.card} key={matchInfo.fixture.id}>
+      <FixturePreviewCard fixtureInfo={matchInfo} />
+    </div>
+  ));
+
+  const isSingle = matchesList.length === 1;
+
+  return (
+    <div className={classes.container}>
+      <div
+        className={[classes.list, isSingle && classes["single-match"]].join(
+          " "
+        )}
+      >
+        {matchesList}
+      </div>
+    </div>
+  );
+};
