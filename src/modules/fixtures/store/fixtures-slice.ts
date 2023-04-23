f4ba46d6eb4befed5dbad6c9fd1f9290870c27d7
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-04-23 13:21:14 +0300

    Delete unused parameter

--- src/modules/fixtures/store/fixtures-slice.ts
+++ src/modules/fixtures/store/fixtures-slice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import { DayFixtures, Fixture } from "api/types/fixtures-types";
 import { fetchFixtures } from "modules/fixtures/store/fixtures-thunk";
 
