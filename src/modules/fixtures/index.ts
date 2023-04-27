2d2fe0b824ba49aab3d0671c60bd35a329798353
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-04-27 09:20:09 +0300

    Added Public Api for fixtures module

---
+++ src/modules/fixtures/index.ts
@@ -0,0 +1,4 @@
+export { fixturesReducer } from "./store/fixtures-slice";
+export { fetchFixtures } from "./store/fixtures-thunk";
+export { FixturesList } from "./components/FixturesList/FixturesList";
+export { PreviewMatchesList } from "modules/fixtures/components/PreviewMatchesList/PreviewMatchesList";
