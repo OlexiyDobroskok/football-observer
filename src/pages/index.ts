79562c578aef3dbbef66839c6950bcd5c56fc7a0
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-04-27 09:19:35 +0300

    Added Public Api for pages

---
+++ src/pages/index.ts
@@ -0,0 +1,7 @@
+export { Root } from "pages/Root/Root";
+export { Home } from "pages/Home/Home";
+export { Calendar } from "pages/Calendar/Calendar";
+export { Standings } from "pages/Standings/Standings";
+export { Profile } from "pages/Profile/Profile";
+export { Teams } from "pages/Teams/Teams";
+export { TeamInfo } from "pages/TeamInfo/TeamInfo";
