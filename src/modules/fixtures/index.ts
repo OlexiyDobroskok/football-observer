647a7ab6cd99a2df23dbe8a7e9ecc132d1903270
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-04-28 16:10:43 +0300

    Replaced import paths to relative for public API from fixtures module

--- src/modules/fixtures/index.ts
+++ src/modules/fixtures/index.ts
@@ -1,4 +1,4 @@
 export { fixturesReducer } from "./store/fixtures-slice";
 export { fetchFixtures } from "./store/fixtures-thunk";
 export { FixturesList } from "./components/FixturesList/FixturesList";
-export { PreviewMatchesList } from "modules/fixtures/components/PreviewMatchesList/PreviewMatchesList";
+export { PreviewMatchesList } from "./components/PreviewMatchesList/PreviewMatchesList";
