781b1da71ec72d06eb9416f5eeea19077a89b7a3
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-04-28 16:11:04 +0300

    Replaced import paths to relative for public API from leagues-filter module

--- src/modules/leagues-filter/index.ts
+++ src/modules/leagues-filter/index.ts
@@ -1,3 +1,3 @@
 export { leaguesReducer } from "./store/leagues-slice";
-export { LeaguesFilter } from "modules/leagues-filter/components/LeaguesFilter/LeaguesFilter";
-export { SeasonsSelect } from "modules/leagues-filter/components/SeasonsSelect/SeasonsSelect";
+export { LeaguesFilter } from "./components/LeaguesFilter/LeaguesFilter";
+export { SeasonsSelect } from "./components/SeasonsSelect/SeasonsSelect";
