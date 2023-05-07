fc93774251de8349c04136310d2f9cbafe865dab
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-05-07 13:05:48 +0300

    Created GoalEvent ui component for fixtures module

---
+++ src/modules/fixtures/ui/GoalEvent/GoalEvent.tsx
@@ -0,0 +1,41 @@
+import { getGoalDescription } from "../../helpers/get-goal-description";
+import { FixtureEventApp } from "api/types/fixtures-types";
+import ballIcon from "./ball.svg";
+import classes from "./GoalEvent.module.scss";
+
+export interface GoalEventProps {
+  events: FixtureEventApp[];
+  teamLocationStatus: "HOME" | "AWAY";
+}
+
+export const GoalEvent = ({ events, teamLocationStatus }: GoalEventProps) => {
+  const [goalEvent] = events;
+  const playerName = goalEvent.player.name;
+
+  let goalDescription = "";
+  if (events.length > 1) {
+    goalDescription = events
+      .map((event) => getGoalDescription(event))
+      .join(", ");
+  }
+
+  if (events.length === 1) {
+    goalDescription = getGoalDescription(goalEvent);
+  }
+
+  return (
+    <div className={classes.event}>
+      <p
+        className={classes.description}
+      >{`${playerName} ${goalDescription}`}</p>
+      <img
+        className={[
+          classes.icon,
+          teamLocationStatus === "AWAY" && classes.away,
+        ].join(" ")}
+        src={ballIcon}
+        alt=""
+      />
+    </div>
+  );
+};
