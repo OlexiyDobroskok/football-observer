a58554bdf57b3d097ae70fc368d11d2abd73fee1
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-05-07 12:40:33 +0300

    Added LG board size

--- src/modules/fixtures/ui/Scoreboard/Scoreboard.tsx
+++ src/modules/fixtures/ui/Scoreboard/Scoreboard.tsx
@@ -3,14 +3,19 @@ import classes from "./Scoreboard.module.scss";
 
 export interface ScoreboardProps {
   matchScore: FixtureGoals;
-  boardSize?: "MD";
+  boardSize?: "MD" | "LG";
 }
 
 export const Scoreboard = ({
   matchScore: { home: homeTeamGoals, away: awayTeamGoals },
   boardSize,
 }: ScoreboardProps) => (
-  <p className={[classes.score, boardSize === "MD" && classes.md].join(" ")}>
+  <p
+    className={[
+      classes.score,
+      boardSize === "MD" ? classes.md : boardSize === "LG" ? classes.lg : "",
+    ].join(" ")}
+  >
     <span>{homeTeamGoals}</span> : <span>{awayTeamGoals}</span>
   </p>
 );
