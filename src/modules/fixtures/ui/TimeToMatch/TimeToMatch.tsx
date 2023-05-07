5659cf1529b4a0d44c6d42993db59159f02a2158
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-05-07 12:57:34 +0300

    Created TimeToMatch ui component for fixtures module

---
+++ src/modules/fixtures/ui/TimeToMatch/TimeToMatch.tsx
@@ -0,0 +1,31 @@
+import { twoDigitsFormat } from "../../helpers/date-format";
+import { useCountDown } from "../../hooks/use-count-down";
+import classes from "./TimeToMatch.module.scss";
+
+export interface TimeToMatchProps {
+  fixtureDate: string;
+}
+
+export const TimeToMatch = ({ fixtureDate }: TimeToMatchProps) => {
+  const { days, hrs, mins, secs } = useCountDown(fixtureDate);
+  return (
+    <div className={classes["count-down"]}>
+      <div className={classes.count}>
+        <div className={classes.digits}>{twoDigitsFormat(days)}</div>
+        <div className={classes.description}>Days</div>
+      </div>
+      <div className={classes.count}>
+        <div className={classes.digits}>{twoDigitsFormat(hrs)}</div>
+        <div className={classes.description}>Hrs</div>
+      </div>
+      <div className={classes.count}>
+        <div className={classes.digits}>{twoDigitsFormat(mins)}</div>
+        <div className={classes.description}>Mins</div>
+      </div>
+      <div className={classes.count}>
+        <div className={classes.digits}>{twoDigitsFormat(secs)}</div>
+        <div className={classes.description}>Secs</div>
+      </div>
+    </div>
+  );
+};
