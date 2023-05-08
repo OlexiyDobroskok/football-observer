fb2abbd1277d8e37133aa638bf4c13f544191f9c
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-05-08 19:47:27 +0300

    Corrected TimeToMatch component after implementation getTimeToMatch helper function to useCountDown hook

--- src/modules/fixtures/ui/TimeToMatch/TimeToMatch.tsx
+++ src/modules/fixtures/ui/TimeToMatch/TimeToMatch.tsx
@@ -7,25 +7,30 @@ export interface TimeToMatchProps {
 }
 
 export const TimeToMatch = ({ fixtureDate }: TimeToMatchProps) => {
-  const { days, hrs, mins, secs } = useCountDown(fixtureDate);
-  return (
-    <div className={classes["count-down"]}>
-      <div className={classes.count}>
-        <div className={classes.digits}>{twoDigitsFormat(days)}</div>
-        <div className={classes.description}>Days</div>
+  const downCounter = useCountDown(fixtureDate);
+
+  if (downCounter) {
+    const { days, hrs, mins, secs } = downCounter;
+    return (
+      <div className={classes["count-down"]}>
+        <div className={classes.count}>
+          <div className={classes.digits}>{twoDigitsFormat(days)}</div>
+          <div className={classes.description}>Days</div>
+        </div>
+        <div className={classes.count}>
+          <div className={classes.digits}>{twoDigitsFormat(hrs)}</div>
+          <div className={classes.description}>Hrs</div>
+        </div>
+        <div className={classes.count}>
+          <div className={classes.digits}>{twoDigitsFormat(mins)}</div>
+          <div className={classes.description}>Mins</div>
+        </div>
+        <div className={classes.count}>
+          <div className={classes.digits}>{twoDigitsFormat(secs)}</div>
+          <div className={classes.description}>Secs</div>
+        </div>
       </div>
-      <div className={classes.count}>
-        <div className={classes.digits}>{twoDigitsFormat(hrs)}</div>
-        <div className={classes.description}>Hrs</div>
-      </div>
-      <div className={classes.count}>
-        <div className={classes.digits}>{twoDigitsFormat(mins)}</div>
-        <div className={classes.description}>Mins</div>
-      </div>
-      <div className={classes.count}>
-        <div className={classes.digits}>{twoDigitsFormat(secs)}</div>
-        <div className={classes.description}>Secs</div>
-      </div>
-    </div>
-  );
+    );
+  }
+  return <p className={classes.description}>Match is Started!</p>;
 };
