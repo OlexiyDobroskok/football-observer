1e4af4145a83e1e9c8e2fe33f1e4651c9236a80a
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-05-10 12:36:40 +0300

    Update H2H Thunk returned value.

--- src/modules/fixtures/store/head-to-head-thunk.ts
+++ src/modules/fixtures/store/head-to-head-thunk.ts
@@ -1,16 +1,36 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { Fixture, HeadToHeadArgs } from "api/types/fixtures-types";
 import { FootballService } from "api/football-service";
+import { getH2HStats } from "../helpers/getH2HStats";
+import { H2HStats } from "./head-to-head-slice";
+
+interface HeadToHeadData {
+  fixtures: Fixture[];
+  stats: H2HStats;
+}
 
 export const fetchHeadToHeadFixtureInfo = createAsyncThunk<
-  Fixture[],
+  HeadToHeadData,
   HeadToHeadArgs,
   { rejectValue: string }
 >(
   "headToHead/fetchHeadToHeadFixtureInfo",
-  async (params, { rejectWithValue }) => {
+  async ({ homeTeamId, awayTeamId }, { rejectWithValue }) => {
     try {
-      return await FootballService.getHeadToHeadFixtureInfo(params);
+      const fixtures = await FootballService.getHeadToHeadFixtureInfo({
+        homeTeamId,
+        awayTeamId,
+      });
+      const descendingSortedFixtures = fixtures
+        .filter(({ goals }) => goals.home !== null && goals.away !== null)
+        .sort(
+          (firstFixture, secondFixture) =>
+            secondFixture.fixture.timestamp - firstFixture.fixture.timestamp
+        );
+
+      const stats = getH2HStats({ homeTeamId, fixtures });
+
+      return { fixtures: descendingSortedFixtures, stats };
     } catch (error) {
       return rejectWithValue((error as Error).message);
     }
