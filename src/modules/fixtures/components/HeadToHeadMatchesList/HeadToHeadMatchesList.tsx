b8d9950f06e75578df56f6a970677d5609664141
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-05-10 12:42:41 +0300

    Created HeadToHeadMatchesList Component for fixtures module

---
+++ src/modules/fixtures/components/HeadToHeadMatchesList/HeadToHeadMatchesList.tsx
@@ -0,0 +1,64 @@
+import { useAppSelector } from "hooks/redux";
+import { FixtureItem } from "../FixtureItem/FixtureItem";
+import {
+  getDateLongFormat,
+  getValidDateTimeStrYMDFormat,
+} from "../../helpers/date-format";
+import { checkIsEven } from "helpers/check-is-even";
+import classes from "./HeadToHeadMatchesList.module.scss";
+export interface HeadToHeadMatchesListProps {
+  numberRecentMatches?: number;
+}
+
+export const HeadToHeadMatchesList = ({
+  numberRecentMatches,
+}: HeadToHeadMatchesListProps) => {
+  const { headToHeadFixtures } = useAppSelector(({ headToHead }) => headToHead);
+
+  if (headToHeadFixtures) {
+    const fixturesList = headToHeadFixtures
+      .slice(0, numberRecentMatches)
+      .map(({ fixture, teams, goals }, index) => {
+        const { id, date, status, venue } = fixture;
+        const matchDate = new Date(date);
+        const formattedDate = getDateLongFormat(matchDate);
+        const stadium = `${venue.name}, ${venue.city}`;
+        return (
+          <li
+            className={[classes.item, checkIsEven(index) && classes.dark].join(
+              " "
+            )}
+            key={id}
+          >
+            <p className={classes.description}>
+              <time dateTime={getValidDateTimeStrYMDFormat(matchDate)}>
+                {formattedDate}
+              </time>
+            </p>
+            <p className={classes.description}>{stadium}</p>
+            <FixtureItem
+              fixtureId={id}
+              date={date}
+              matchStatus={status.short}
+              teamsOfMatch={teams}
+              matchScore={goals}
+            />
+          </li>
+        );
+      });
+
+    return (
+      <section className={classes["recent-meetings"]}>
+        <h3 className={classes.title}>Recent Meetings</h3>
+        <ul className={classes.list}>{fixturesList}</ul>
+      </section>
+    );
+  }
+
+  return (
+    <section>
+      <h3 className={classes.title}>Recent Meetings</h3>
+      <p className={classes.message}>Not Found!</p>
+    </section>
+  );
+};
