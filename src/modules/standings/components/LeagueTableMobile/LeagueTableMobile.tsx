59a6159a827dc854f838e9b3eea826b8437e31ca
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-05-11 20:07:15 +0300

    Deleted props for LeagueTableMobile. Data is now obtained from the global state

--- src/modules/standings/components/LeagueTableMobile/LeagueTableMobile.tsx
+++ src/modules/standings/components/LeagueTableMobile/LeagueTableMobile.tsx
@@ -7,67 +7,72 @@ import { fetchLeagueStandings } from "../../store/standings-thunk";
 import { PositionRowMobile } from "../../ui/PositionRowMobile/PositionRowMobile";
 import classes from "./LeagueTableMobile.module.scss";
 
-interface LeagueTableMobileProps {
-  leagueId: number;
-  season: number;
-}
-
-export const LeagueTableMobile = ({
-  leagueId,
-  season,
-}: LeagueTableMobileProps) => {
+export const LeagueTableMobile = () => {
   const { leagueData, isLoading } = useAppSelector(
     ({ standings }) => standings
+  );
+  const { currentLeagueId, currentSeason } = useAppSelector(
+    ({ leagues }) => leagues
   );
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(fetchLeagueStandings({ leagueId: leagueId, season: season }));
-  }, [leagueId, season]);
+    if (currentLeagueId && currentSeason)
+      dispatch(
+        fetchLeagueStandings({
+          leagueId: currentLeagueId,
+          season: currentSeason,
+        })
+      );
+  }, [currentLeagueId, currentSeason]);
 
-  const positionsList = leagueData?.standings?.map(
-    (
-      {
-        team: { id, logo, name },
-        rank,
-        all: { played },
-        goalsDiff,
-        points,
-        form,
-      },
-      index
-    ) => (
-      <PositionRowMobile
-        key={id}
-        id={id}
-        rank={rank}
-        name={name}
-        logo={logo}
-        gamesPlayed={played}
-        goalsDiff={goalsDiff}
-        points={points}
-        formList={form}
-        isEven={checkIsEven(index)}
-      />
-    )
-  );
+  if (leagueData) {
+    const positionsList = leagueData.standings.map(
+      (
+        {
+          team: { id, logo, name },
+          rank,
+          all: { played },
+          goalsDiff,
+          points,
+          form,
+        },
+        index
+      ) => (
+        <PositionRowMobile
+          key={id}
+          id={id}
+          rank={rank}
+          name={name}
+          logo={logo}
+          gamesPlayed={played}
+          goalsDiff={goalsDiff}
+          points={points}
+          formList={form}
+          isEven={checkIsEven(index)}
+        />
+      )
+    );
 
-  const headingsList = tableHeaders.map(({ id, head }) => (
-    <th key={id}>{head}</th>
-  ));
+    const headingsList = tableHeaders.map(({ id, head }) => (
+      <th key={id}>{head}</th>
+    ));
 
-  return (
-    <>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <table className={classes.table}>
-          <thead>
-            <tr>{headingsList}</tr>
-          </thead>
-          <tbody>{positionsList}</tbody>
-        </table>
-      )}
-    </>
-  );
+    return (
+      <>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <table className={classes.table}>
+            <thead>
+              <tr>{headingsList}</tr>
+            </thead>
+            <tbody>{positionsList}</tbody>
+          </table>
+        )}
+      </>
+    );
+  }
+
+  return <p className={classes.message}>League Standings Not Found...</p>;
 };
