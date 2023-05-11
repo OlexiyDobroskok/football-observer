4e82452f71d867b59ea81c0e7a36171bcc969483
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-05-11 19:47:53 +0300

    Moved the logic from TopLeaguesList component to leaguesSlice.

--- src/modules/leagues-filter/components/TopLeaguesList/TopLeaguesList.tsx
+++ src/modules/leagues-filter/components/TopLeaguesList/TopLeaguesList.tsx
@@ -1,58 +1,55 @@
-import { useMemo } from "react";
 import { useAppDispatch, useAppSelector } from "hooks/redux";
 import { List } from "ui/List/List";
-import { LeagueInformation } from "api/types/leagues-types";
 import { setCurrentLeague } from "../../store/leagues-slice";
 import { LeagueFilterButton } from "../../ui/LeagueFilterButton/LeagueFilterButton";
 import classes from "./TopLeaguesList.module.scss";
 
-const topLeaguesIdList = [39, 78, 140, 135, 61];
-
 export const TopLeaguesList = () => {
-  const { availableLeagues, currentLeagueId } = useAppSelector(
-    ({ leagues }) => leagues
-  );
+  const { topLeaguesInfo, currentLeagueIsTop, currentLeagueInformation } =
+    useAppSelector(({ leagues }) => leagues);
   const dispatch = useAppDispatch();
 
-  const topLeagueId = useMemo(
-    () => topLeaguesIdList.find((id) => id === currentLeagueId),
-    [currentLeagueId]
-  );
+  if (topLeaguesInfo && currentLeagueInformation) {
+    const {
+      league: {
+        id: currentLeagueId,
+        name: currentLeagueName,
+        logo: currentLeagueLogo,
+      },
+    } = currentLeagueInformation;
 
-  const idList = topLeagueId
-    ? topLeaguesIdList
-    : [currentLeagueId, ...topLeaguesIdList];
+    const handleTopLeagueButtonClick = (id: number) => {
+      dispatch(setCurrentLeague(id));
+    };
 
-  const topLeaguesList = useMemo(
-    () =>
-      idList
-        .map((id) => availableLeagues?.filter(({ league }) => league.id === id))
-        .flat()
-        .filter(
-          (leagueInfo) => leagueInfo !== undefined
-        ) as LeagueInformation[],
-    [availableLeagues, idList]
-  );
-
-  const handleTopLeagueButtonClick = (id: number) => {
-    dispatch(setCurrentLeague(id));
-  };
-
-  return (
-    <div className={classes["List-wrap"]}>
-      <List
-        className={classes.list}
-        listItems={topLeaguesList}
-        renderItem={({ league: { id, name, logo } }) => (
-          <LeagueFilterButton
-            key={id}
-            leagueName={name}
-            logo={logo}
-            isCurrent={id === currentLeagueId}
-            onClick={() => handleTopLeagueButtonClick(id)}
+    return (
+      <div className={classes["list-wrap"]}>
+        <div className={classes["leagues-list"]}>
+          {!currentLeagueIsTop && (
+            <LeagueFilterButton
+              leagueName={currentLeagueName}
+              logo={currentLeagueLogo}
+              isCurrent={true}
+              onClick={() => handleTopLeagueButtonClick(currentLeagueId)}
+            />
+          )}
+          <List
+            className={classes["top-list"]}
+            listItems={topLeaguesInfo}
+            renderItem={({ league: { id, name, logo } }) => (
+              <LeagueFilterButton
+                key={id}
+                leagueName={name}
+                logo={logo}
+                isCurrent={id === currentLeagueId}
+                onClick={() => handleTopLeagueButtonClick(id)}
+              />
+            )}
           />
-        )}
-      />
-    </div>
-  );
+        </div>
+      </div>
+    );
+  }
+
+  return <p className={classes.message}>Available Leagues Not Found...</p>;
 };
--- src/modules/leagues-filter/store/leagues-slice.ts
+++ src/modules/leagues-filter/store/leagues-slice.ts
@@ -3,10 +3,15 @@ import { LeagueInformation, SeasonDefinition } from "api/types/leagues-types";
 import { fetchLeagues } from "./leagues-thunk";
 import { RequestStatus } from "api/types/global";
 
+const topLeaguesIdList = [39, 78, 140, 135, 61];
+
 export interface LeaguesState {
   availableLeagues: LeagueInformation[] | undefined;
   availableSeasons: SeasonDefinition[] | undefined;
+  topLeaguesInfo: LeagueInformation[] | undefined;
   currentLeagueId: number;
+  currentLeagueIsTop: boolean;
+  currentLeagueInformation: LeagueInformation | undefined;
   currentSeason: number | undefined;
   searchQuery: string;
   filteredLeagues: LeagueInformation[];
@@ -18,7 +23,10 @@ export interface LeaguesState {
 const initialState: LeaguesState = {
   availableLeagues: undefined,
   availableSeasons: undefined,
+  topLeaguesInfo: undefined,
   currentLeagueId: 39,
+  currentLeagueIsTop: true,
+  currentLeagueInformation: undefined,
   currentSeason: undefined,
   searchQuery: "",
   filteredLeagues: [],
@@ -33,10 +41,17 @@ const leaguesSlice = createSlice({
   reducers: {
     setCurrentLeague: (state, { payload }: PayloadAction<number>) => {
       state.currentLeagueId = payload;
+      const isTopLeague = topLeaguesIdList.find(
+        (topLeagueId) => topLeagueId === payload
+      );
+      state.currentLeagueIsTop = !!isTopLeague;
+
       if (state.availableLeagues) {
         const leagueInformation = state.availableLeagues.find(
-          ({ league: { id } }) => id === state.currentLeagueId
+          ({ league: { id } }) => id === payload
         );
+        state.currentLeagueInformation = leagueInformation;
+
         const leagueSeasons = leagueInformation
           ? leagueInformation.seasons.sort(
               (seasonFirst, seasonSecond) =>
@@ -51,6 +66,7 @@ const leaguesSlice = createSlice({
     },
     setSearchLeaguesQuery: (state, { payload }: PayloadAction<string>) => {
       state.searchQuery = payload;
+
       if (state.availableLeagues) {
         const minLength = 3;
         state.filteredLeagues =
@@ -73,10 +89,16 @@ const leaguesSlice = createSlice({
       })
       .addCase(fetchLeagues.fulfilled, (state, { payload }) => {
         state.reqStatus = "succeeded";
+        state.availableLeagues = payload;
+        state.isLoading = false;
+        state.error = null;
+
         if (payload.length) {
           const currentLeagueInformation = payload.find(
             ({ league: { id } }) => id === state.currentLeagueId
           );
+          state.currentLeagueInformation = currentLeagueInformation;
+
           const leagueSeasons = currentLeagueInformation
             ? currentLeagueInformation.seasons
                 .filter(({ coverage }) => coverage.standings)
@@ -87,10 +109,13 @@ const leaguesSlice = createSlice({
             : [];
           state.availableSeasons = leagueSeasons;
           state.currentSeason = leagueSeasons[leagueSeasons.length - 1]?.year;
+
+          state.topLeaguesInfo = topLeaguesIdList
+            .map((topLeagueId) =>
+              payload.filter(({ league }) => league.id === topLeagueId)
+            )
+            .flat();
         }
-        state.isLoading = false;
-        state.error = null;
-        state.availableLeagues = payload;
       })
       .addCase(fetchLeagues.rejected, (state, { payload, error }) => {
         state.reqStatus = "failed";
