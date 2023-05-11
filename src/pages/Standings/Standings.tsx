1057ab417df55604a6aa19f2083e04c12b72d5a6
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-05-11 20:12:28 +0300

    Moved logic from Standings page to standingsSlice

--- src/pages/Standings/Standings.tsx
+++ src/pages/Standings/Standings.tsx
@@ -1,4 +1,3 @@
-import { useMemo } from "react";
 import { useAppSelector } from "hooks/redux";
 import { SeasonsSelect } from "modules/leagues-filter";
 import { LeagueTableMobile } from "modules/standings";
@@ -7,27 +6,27 @@ import { Container } from "ui/Container/Container";
 import classes from "./Standings.module.scss";
 
 export const Standings = () => {
-  const { currentLeagueId, currentSeason, availableLeagues } = useAppSelector(
-    ({ leagues }) => leagues
-  );
-
-  const currentLeagueInformation = useMemo(
-    () => availableLeagues?.find(({ league }) => league.id === currentLeagueId),
-    [availableLeagues, currentLeagueId]
-  );
+  const { currentLeagueInformation } = useAppSelector(({ leagues }) => leagues);
 
   const leagueName = currentLeagueInformation
     ? `${currentLeagueInformation.league.name} (${currentLeagueInformation.country.name}) `
-    : "League information not found!";
+    : "League Information Not Found...";
 
   return (
     <>
       <HiddenElement as="h1">League Standings</HiddenElement>
-      <Container className={classes.container}>
-        <h2 className={classes.title}>{leagueName}</h2>
-        <SeasonsSelect />
-      </Container>
-      <LeagueTableMobile leagueId={currentLeagueId} season={currentSeason} />
+      {currentLeagueInformation && (
+        <>
+          <Container className={classes.container}>
+            <h2 className={classes.title}>{leagueName}</h2>
+            <SeasonsSelect />
+          </Container>
+          <LeagueTableMobile />
+        </>
+      )}
+      {!currentLeagueInformation && (
+        <p className={classes.title}>{leagueName}</p>
+      )}
     </>
   );
 };
