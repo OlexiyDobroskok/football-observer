728bb13659791baf213d38c1856477459bc56db9
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-05-12 17:41:33 +0300

    Added getting data from inside the FixtureDetailCard  component

--- src/modules/fixtures/components/FixtureDetailCard/FixtureDetailCard.tsx
+++ src/modules/fixtures/components/FixtureDetailCard/FixtureDetailCard.tsx
@@ -1,4 +1,6 @@
-import { useAppSelector } from "hooks/redux";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "hooks/redux";
 import { TeamEvents } from "../TeamEvents/TeamEvents";
 import { RefereeInfo } from "../../ui/RefereeInfo/RefereeInfo";
 import { StadiumInfo } from "../../ui/StadiumInfo/StadiumInfo";
@@ -12,12 +14,19 @@ import { PreviewTeamMark } from "../../ui/PreviewTeamMark/PreviewTeamMark";
 import { Scoreboard } from "../../ui/Scoreboard/Scoreboard";
 import { TimeToMatch } from "../../ui/TimeToMatch/TimeToMatch";
 import { MatchStatus } from "../../ui/MatchStatus/MatchStatus";
+import { fetchFixtureDetail } from "../../store/fixture-detail-thunk";
 import classes from "./FixtureDetailCard.module.scss";
 
 export const FixtureDetailCard = () => {
+  const { fixtureId } = useParams<"fixtureId">();
+  const dispatch = useAppDispatch();
   const { fixtureDetail, isLive, isScheduled } = useAppSelector(
     ({ fixtureDetail }) => fixtureDetail
   );
+
+  useEffect(() => {
+    if (fixtureId) dispatch(fetchFixtureDetail({ fixtureId }));
+  }, [fixtureId]);
 
   if (fixtureDetail) {
     const { fixture, teams, goals, events } = fixtureDetail;
