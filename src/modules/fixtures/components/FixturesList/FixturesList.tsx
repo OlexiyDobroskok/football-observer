3ee909fd252fdad2aee19f4c8d2ad201a685a000
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-05-13 15:16:16 +0300

    Updated FixturesList component. Data is now obtained from the global store inside the component

--- src/modules/fixtures/components/FixturesList/FixturesList.tsx
+++ src/modules/fixtures/components/FixturesList/FixturesList.tsx
@@ -1,4 +1,6 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "hooks/redux";
+import { fetchFixtures } from "../../store/fixtures-thunk";
 import { useInfinityPagination } from "hooks/use-infinity-pagination";
 import { checkIsEven } from "helpers/check-is-even";
 import { FixtureItem } from "../FixtureItem/FixtureItem";
@@ -6,19 +8,41 @@ import {
   getDateLongFormat,
   getValidDateTimeStrYMDFormat,
 } from "../../helpers/date-format";
-import { DayFixtures } from "../../helpers/day-fixtures-converter";
+import { MatchesType } from "../../types/types";
 import classes from "./FixturesList.module.scss";
 
 export interface FixturesListProps {
-  fixtures: DayFixtures[];
+  fixturesType: Exclude<MatchesType, "LIVE">;
 }
 
-export const FixturesList = ({ fixtures }: FixturesListProps) => {
+export const FixturesList = ({ fixturesType }: FixturesListProps) => {
+  const { currentLeagueId, currentSeason } = useAppSelector(
+    ({ leagues }) => leagues
+  );
+  const { scheduledMatches, finishedMatches } = useAppSelector(
+    ({ fixtures }) => fixtures
+  );
+  const dispatch = useAppDispatch();
+
+  const fixturesList =
+    !!scheduledMatches && fixturesType === "SCHEDULED"
+      ? scheduledMatches
+      : finishedMatches
+      ? finishedMatches
+      : [];
+
   const [containerRef, fixturesPage] = useInfinityPagination({
-    dataList: fixtures,
+    dataList: fixturesList,
     elementsPerPage: 7,
     observerOptions: { threshold: 0 },
   });
+
+  useEffect(() => {
+    if (currentLeagueId && currentSeason)
+      dispatch(
+        fetchFixtures({ leagueId: currentLeagueId, season: currentSeason })
+      );
+  }, [currentLeagueId, currentSeason]);
 
   const matchesList = fixturesPage.map(({ date, fixtures }) => {
     const matchesDate = new Date(date);
@@ -47,5 +71,24 @@ export const FixturesList = ({ fixtures }: FixturesListProps) => {
     );
   });
 
-  return <div className={classes["fixtures-list"]}>{matchesList}</div>;
+  const emptyMessage =
+    fixturesType === "FINISHED"
+      ? "Sorry, no matches were played..."
+      : "Sorry, there are no scheduled matches...";
+
+  const isMatchesData = !!finishedMatches && !!scheduledMatches;
+
+  return (
+    <>
+      {!!matchesList.length && (
+        <div className={classes["fixtures-list"]}>{matchesList}</div>
+      )}
+      {!matchesList.length && isMatchesData && (
+        <p className={classes.message}>{emptyMessage}</p>
+      )}
+      {!isMatchesData && (
+        <p className={classes.message}>Sorry, Matches Not Found...</p>
+      )}
+    </>
+  );
 };
