7819690a6a234f37b251050e75056b5822e40281
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-05-13 22:08:09 +0300

    Created fn for sorting players by position

---
+++ src/modules/team/helpers/sort-players-by-position.ts
@@ -0,0 +1,27 @@
+import { TeamPlayerDefinition } from "api/types/team-types";
+import { SortedTeamSquad } from "../types/types";
+import { playerPositions } from "api/helpers/consts";
+
+export const sortPlayersByPosition = (
+  players: TeamPlayerDefinition[]
+): SortedTeamSquad => {
+  const squad: SortedTeamSquad = {
+    goalkeepers: [],
+    defenders: [],
+    midfielders: [],
+    attackers: [],
+  };
+
+  players.forEach((player) => {
+    if (player.position.toLowerCase() === playerPositions.GK.toLowerCase())
+      squad.goalkeepers = [...squad.goalkeepers, player];
+    if (player.position.toLowerCase() === playerPositions.DEF.toLowerCase())
+      squad.defenders = [...squad.defenders, player];
+    if (player.position.toLowerCase() === playerPositions.MF.toLowerCase())
+      squad.midfielders = [...squad.midfielders, player];
+    if (player.position.toLowerCase() === playerPositions.ATT.toLowerCase())
+      squad.attackers = [...squad.attackers, player];
+  });
+
+  return squad;
+};
