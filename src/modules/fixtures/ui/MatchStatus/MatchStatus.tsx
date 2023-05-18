c37e4512b722e5006bdba4e90ceb8a2c5e791573
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-05-18 20:42:34 +0300

    Deleted isLive prop for MatchStatus component

--- src/modules/fixtures/ui/MatchStatus/MatchStatus.tsx
+++ src/modules/fixtures/ui/MatchStatus/MatchStatus.tsx
@@ -1,22 +1,24 @@
 import { FixtureStatus } from "api/types/fixtures-types";
+import { checkIsMatchLive } from "../../helpers/checkIsMatchLive";
 import classes from "./MatchStatus.module.scss";
 
 export interface MatchStatusProps {
   matchStatus: FixtureStatus;
-  isLive: boolean;
 }
 
 export const MatchStatus = ({
   matchStatus: { short: matchStatus, elapsed: elapsedTime },
-  isLive,
-}: MatchStatusProps) => (
-  <div className={classes.container}>
-    {isLive && (
-      <>
-        <p className={classes.time}>{`${elapsedTime}'`}</p>
-        <p className={classes.status}>({matchStatus})</p>
-      </>
-    )}
-    {!isLive && <p className={classes["status-lg"]}>{matchStatus}</p>}
-  </div>
-);
+}: MatchStatusProps) => {
+  const isLive = checkIsMatchLive(matchStatus);
+  return (
+    <div className={classes.container}>
+      {isLive && (
+        <>
+          <p className={classes.time}>{`${elapsedTime}'`}</p>
+          <p className={classes.status}>({matchStatus})</p>
+        </>
+      )}
+      {!isLive && <p className={classes["status-lg"]}>{matchStatus}</p>}
+    </div>
+  );
+};
