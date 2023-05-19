a12aef8967b5fa2f15ffbde495f367a60b5802d4
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-05-19 12:19:03 +0300

    Created useFixtureDetail CustomHook

---
+++ src/modules/fixtures/hooks/use-fixture-detail.ts
@@ -0,0 +1,35 @@
+import { useEffect, useRef } from "react";
+import { useParams } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "hooks/redux";
+import { fetchFixtureDetail } from "../store/fixture-detail-thunk";
+import { resetFixtureDetailReqStatus } from "../store/fixture-detail-slice";
+
+export const useFixtureDetail = () => {
+  const { fixtureId } = useParams<"fixtureId">();
+  const fixtureDetailState = useAppSelector(
+    ({ fixtureDetail }) => fixtureDetail
+  );
+  const { isLive } = fixtureDetailState;
+  const dispatch = useAppDispatch();
+  const intervalIdRef = useRef<number | null>(null);
+
+  useEffect(() => {
+    if (fixtureId) {
+      dispatch(fetchFixtureDetail({ fixtureId }));
+
+      if (isLive) {
+        intervalIdRef.current = window.setInterval(() => {
+          dispatch(resetFixtureDetailReqStatus());
+          dispatch(fetchFixtureDetail({ fixtureId }));
+        }, 30000);
+      }
+    }
+
+    return () => {
+      if (isLive) dispatch(resetFixtureDetailReqStatus());
+      if (intervalIdRef.current) window.clearInterval(intervalIdRef.current);
+    };
+  }, [fixtureId, isLive]);
+
+  return fixtureDetailState;
+};
