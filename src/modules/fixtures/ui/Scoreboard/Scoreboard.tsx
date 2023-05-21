56ba01fc80ca5a9494a3c70f3e19620b439f2ee8
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-05-21 13:34:28 +0300

    Created scoreBoardSize const for Scoreboard component. Moved classes with logic to the body of the component

--- src/modules/fixtures/ui/Scoreboard/Scoreboard.tsx
+++ src/modules/fixtures/ui/Scoreboard/Scoreboard.tsx
@@ -1,21 +1,30 @@
 import { FixtureGoals } from "api/types/fixtures-types";
 import classes from "./Scoreboard.module.scss";
 
+export const scoreBoardSize = {
+  md: "MD",
+  lg: "LG",
+} as const;
+
+export type BoardSize = (typeof scoreBoardSize)[keyof typeof scoreBoardSize];
+
 export interface ScoreboardProps {
   matchScore: FixtureGoals;
-  boardSize?: "MD" | "LG";
+  boardSize?: BoardSize;
 }
 
 export const Scoreboard = ({
   matchScore: { home: homeTeamGoals, away: awayTeamGoals },
   boardSize,
-}: ScoreboardProps) => (
-  <p
-    className={[
-      classes.score,
-      boardSize === "MD" ? classes.md : boardSize === "LG" ? classes.lg : "",
-    ].join(" ")}
-  >
-    <span>{homeTeamGoals}</span> : <span>{awayTeamGoals}</span>
-  </p>
-);
+}: ScoreboardProps) => {
+  const scoreClassName = [
+    classes.score,
+    boardSize === "MD" ? classes.md : boardSize === "LG" ? classes.lg : "",
+  ].join(" ");
+
+  return (
+    <p className={scoreClassName}>
+      <span>{homeTeamGoals}</span> : <span>{awayTeamGoals}</span>
+    </p>
+  );
+};
