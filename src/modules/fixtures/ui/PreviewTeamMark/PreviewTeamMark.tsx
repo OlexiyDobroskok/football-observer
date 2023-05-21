477dabdaf110f6b7c7310f4f1e71dd9af5f6db7b
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-05-21 13:39:43 +0300

    Added Link to team information for PreviewTeamMark component

--- src/modules/fixtures/ui/PreviewTeamMark/PreviewTeamMark.tsx
+++ src/modules/fixtures/ui/PreviewTeamMark/PreviewTeamMark.tsx
@@ -1,15 +1,23 @@
 import { FixtureTeam } from "api/types/fixtures-types";
 import classes from "./PreviewTeamMark.module.scss";
+import { Link } from "react-router-dom";
 
 export interface PreviewTeamMarkProps {
   team: FixtureTeam;
 }
 
 export const PreviewTeamMark = ({
-  team: { name, logo },
+  team: { id, name, logo },
 }: PreviewTeamMarkProps) => (
   <div className={classes.team}>
     <img className={classes.teamLogo} src={logo} alt="" />
-    <p className={classes.teamName}>{name}</p>
+    <p className={classes.teamName} id={name}>
+      {name}
+    </p>
+    <Link
+      className={classes.teamLink}
+      to={`/teams/${id}`}
+      aria-labelledby={name}
+    />
   </div>
 );
