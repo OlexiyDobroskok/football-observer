f4b7de637706aa278ded55dffe6a9503b751e835
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-05-21 13:45:02 +0300

    Created RedCardEvent component of the fixtures module

---
+++ src/modules/fixtures/ui/RedCardEvent/RedCardEvent.tsx
@@ -0,0 +1,45 @@
+import { FixturePlayerEvents } from "../../types/types";
+import { locationStatus, LocationStatus } from "api/helpers/consts";
+import { getGoalDescription } from "../../helpers/event-description";
+import classes from "./RedCardEvent.module.scss";
+
+export interface RedCardEventProps {
+  eventPlayer: FixturePlayerEvents;
+  teamLocationStatus: LocationStatus;
+}
+
+export const RedCardEvent = ({
+  eventPlayer: { player, events },
+  teamLocationStatus,
+}: RedCardEventProps) => {
+  const [{ time, detail }] = events;
+  const playerName = player.name ? player.name : "Unknown";
+
+  const redCardDescription = getGoalDescription({ time, detail });
+
+  const isAwayTeam = teamLocationStatus === locationStatus.away;
+
+  const redCardEventClassName = [
+    classes.redCardEvent,
+    isAwayTeam && classes.awayTeamEvent,
+  ].join(" ");
+
+  const descriptionClassName = [
+    classes.redCardDescription,
+    isAwayTeam && classes.leftText,
+  ].join(" ");
+
+  const iconClassName = [
+    classes.redCardIcon,
+    isAwayTeam && classes.reverse,
+  ].join(" ");
+
+  return (
+    <div className={redCardEventClassName}>
+      <p
+        className={descriptionClassName}
+      >{`${playerName} ${redCardDescription}`}</p>
+      <div className={iconClassName}></div>
+    </div>
+  );
+};
