d59f53e880b3a8c55cbcdd2da3a584fdd68367bc
Olexii Dobroskok <a.v.dobroskok.kh@gmail.com>
2023-05-26 10:49:47 +0300

    Implemented extra time display for the MatchStatus component of the fixtures module

--- src/modules/fixtures/ui/MatchStatus/MatchStatus.tsx
+++ src/modules/fixtures/ui/MatchStatus/MatchStatus.tsx
@@ -1,20 +1,46 @@
-import { FixtureStatus } from "api/types/fixtures-types";
+import { FixturePeriods, FixtureStatus } from "api/types/fixtures-types";
 import { checkIsMatchLive } from "../../helpers/check-is-match-live";
+import { fixtureStatus } from "api/helpers/consts";
+import { getExtraMinute } from "../../helpers/get-extra-minute";
 import classes from "./MatchStatus.module.scss";
 
 export interface MatchStatusProps {
   matchStatus: FixtureStatus;
+  periods: FixturePeriods;
 }
 
 export const MatchStatus = ({
   matchStatus: { short: matchStatus, elapsed: elapsedTime },
+  periods,
 }: MatchStatusProps) => {
   const isLive = checkIsMatchLive(matchStatus);
+  const isFirstTime =
+    !!periods.first &&
+    matchStatus.toLowerCase() === fixtureStatus["1H"].toLowerCase();
+  const isSecondTime =
+    !!periods.second &&
+    matchStatus.toLowerCase() === fixtureStatus["2H"].toLowerCase();
+  const isHalfTime =
+    matchStatus.toLowerCase() === fixtureStatus.HT.toLowerCase();
+  const firstPeriodExtraTime = getExtraMinute({
+    periodStartTime: periods.first,
+  });
+  const secondPeriodExtraTime = getExtraMinute({
+    periodStartTime: periods.first,
+  });
+
+  const matchTime =
+    isFirstTime && firstPeriodExtraTime
+      ? `${elapsedTime}'+${firstPeriodExtraTime}'`
+      : isSecondTime && secondPeriodExtraTime
+      ? `${elapsedTime}'+${secondPeriodExtraTime}'`
+      : `${elapsedTime}'`;
+
   return (
     <div className={classes.matchInfo}>
       {isLive && (
         <>
-          <span className={classes.time}>{`${elapsedTime}'`}</span>
+          {!isHalfTime && <span className={classes.time}>{matchTime}</span>}
           <span className={classes.status}>({matchStatus})</span>
         </>
       )}
